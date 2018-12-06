2d35a46a219a94f05afddd3f692bd6a18db7f5a9
deepak <marnenideepak4@gmail.com>
2018-12-07 00:55:09 +0530

    second commit

--- ThreadSample/src/com/sample/MyExecutor.java
+++ ThreadSample/src/com/sample/MyExecutor.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-class SampleRunnable implements Runnable{
+class SampleRunnable implements Runnable{ 
 	@Override
 	public void run() {
 		System.out.println(Thread.currentThread().getName() + " (Start) " );
